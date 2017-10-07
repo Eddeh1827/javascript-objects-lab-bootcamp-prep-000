f4b9f478ddf197de0f7801ae25d59150d18eb76e
Eddie Bickham <bickhame@gmail.com>
2017-10-07 05:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return recipes;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[prop2] = 'Cheese',
+  recipes[prop] = 'Cheese',
   recipes;
 }
