026a59ce56536280efb3dd7b8fe7259c4219e740
Eddie Bickham <bickhame@gmail.com>
2017-10-07 05:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = new Object();
 }
-function updateObjectWithKeyAndValue(object, key, value) {
-  recipes['butter', '1/2 stick']
-  return recipes;
+function updateObjectWithKeyAndValue(object, key, value){
+ return Object.assign({},object,{[key]: value})
+
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
